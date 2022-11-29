a3b9d326402b4a8cdf5a3b50da4a4b6932fdf899
SunghaeAn <ahshss903@gmail.com>
2022-11-29 23:04:05 +0900

    Router 처리

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const router = require('./router/main') (app);
 const port = 80;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
