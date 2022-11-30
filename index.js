e125c4e80900295bd5bae1bebb33092472e1e1eb
SunghaeAn <ahshss903@gmail.com>
2022-11-30 20:54:34 +0900

    Fix Css

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const router = require('./router/main') (app);
 const port = 80;
 
+app.use('/', express.static('./views'));
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
